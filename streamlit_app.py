8dc725d226b736c3caba7f126bc2f3a190b4f050
ismulazham <154808783+ismulazham@users.noreply.github.com>
2025-01-08 15:25:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.image("logo.rhaacozz.png", caption='LOGO', use_column_width=True)
+st.image("logo.rhaacozz.png", use_container_width=True)
 st.title("RHAA COZZ")
 st.write(
     "Lihat dan Saksikan Vidio Vidio Menarik Dari Kami [RHAACOZZ](https://www.instagram.com/rhaacozz?igsh=dGY4aHprNmJsMmI0)."
