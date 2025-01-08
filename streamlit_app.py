1714554588ac19ea1bd8d6c84882033197f1ebe8
ismulazham <154808783+ismulazham@users.noreply.github.com>
2025-01-08 14:59:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Happy New Years")
+st.title("🎈NEW")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
