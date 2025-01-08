73796a2ce9337045ebe701f6c6df2768a7cdc787
ismulazham <154808783+ismulazham@users.noreply.github.com>
2025-01-08 15:37:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title("RHAA COZZ")
 st.write(
     "Lihat dan Saksikan Vidio Vidio Menarik Dari Kami [RHAACOZZ](https://www.instagram.com/rhaacozz?igsh=dGY4aHprNmJsMmI0)."
 )
-
+col1=st.columns(1)
+with col1:
+    st.header("Kolom 1")
+    st.write("RHAA COZZ")
